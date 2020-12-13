60c4611e0290ec5c334ee5e86ded83f0027f2f78
Error44-Developer <75933735+Error44-Developer@users.noreply.github.com>
2020-12-13 12:49:26 +0100

    Update main.py

--- Sakura/main.py
+++ Sakura/main.py
@@ -1,1 +1,24 @@
-t
+import discord
+import asyncio
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='') # Dein Prefix
+bot.remove_command('help')
+
+
+@bot.event
+async def on_ready():
+    print('Eingeloggt {}'.format(bot.user.name))
+    bot.loop.create_task(status_task())
+
+
+async def status_task():
+    while True:
+        await bot.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.listening,
+                                      name="")) # Status
+        await asyncio.sleep(20)
+
+
+bot.run('') # Bot Token
